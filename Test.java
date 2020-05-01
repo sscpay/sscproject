e39dd3119276030131071b893db067b1f275bc5c
srinivas <srenivas35@gmail.com>
2020-05-01 13:58:09 +0530

    adding m2() method functionality
    adding m2() method substraction functionality in Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 class Test{
+int sresult=0;
 public static void main(String ... args){
 System.out.println("Hello World");
+sresult=m2(10,15);
+System.out.println("The Substraction result if : "+sresult);
 System.out.println("This is Scratch Project")
-
+}
+public void m2(int a,int b){
+int c=a-b;
+return c;
 }
 }
