d0775b8964ed929d2631c35e0e506480b501d85e
srinivas <srenivas35@gmail.com>
2020-05-01 13:39:04 +0530

    Added m1 addition Functionality in Test.java
    This m1() method is responsible for adding two numbers and returning the result to the main method and it's printing result.

--- Test.java
+++ Test.java
@@ -1,7 +1,15 @@
 class Test{
 public static void main(String ... args){
+int results =0;
 System.out.println("Hello World");
+result=m1(10,15);
+System.out.println("The result of addition is : "+result);
 System.out.println("This is Scratch Project")
 System.out.println("Master Branch Code added New Functionality")
+}
+public static int void m1(int a , int b){
+  int c =0;
+  c=a+b;
+  return c;
 }
 }
